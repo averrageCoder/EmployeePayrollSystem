ddb7f4326cae8ee19877213c0270968a34e708fc
Harsh R Jain <rjainharsh@gmail.com>
2021-09-19 00:08:08 +0530

    ADDED: ability to read and write employee payroll info on console

---
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.employeepayroll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+	
+public class EmployeePayrollService {
+	
+	public enum I0Service {CONSOLEIO, FILE_I0, DB_I0, REST_I0}
+	private List<EmployeePayrollData> employeePayrollList;
+	
+	public EmployeePayrollService(ArrayList<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	public static void main(String[] args) {	
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList) ;
+		Scanner consoleInputReader = new Scanner(System.in) ;
+		employeePayrollService.readEmployeePayrollData(consoleInputReader) ;
+		employeePayrollService.writeEmployeePayrollData();
+	}
+
+	private void writeEmployeePayrollData() {	
+		System.out.println("\nWriting Employee Payroll to Console\n" + employeePayrollList) ;
+	}
+
+	private void readEmployeePayrollData(Scanner consoleInputReader) {	
+		System.out.println("Enter Employee ID: ");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee Name: ");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));	
+	}
+}
