be6deb7dde09d34bee9c0d5245d55a56e18b3ea4
Harsh R Jain <rjainharsh@gmail.com>
2021-09-19 02:48:02 +0530

    MODIFIED: to string method for easier parsing

--- lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
@@ -14,7 +14,7 @@ public class EmployeePayrollData {
 
 	@Override
 	public String toString() {
-		return "EmployeePayrollData [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+		return "id=" + id + ", name=" + name + ", salary=" + salary;
 	}
 	
 }
