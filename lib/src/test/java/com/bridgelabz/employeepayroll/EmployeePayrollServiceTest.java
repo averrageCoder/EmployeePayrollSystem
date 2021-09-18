baed0fbbe2bcb0fa6c41a1a633e84cbef367a1b4
Harsh R Jain <rjainharsh@gmail.com>
2021-09-19 02:49:16 +0530

    ADDED: testcase to verify if the file is parsed properly

--- lib/src/test/java/com/bridgelabz/employeepayroll/EmployeePayrollServiceTest.java
+++ lib/src/test/java/com/bridgelabz/employeepayroll/EmployeePayrollServiceTest.java
@@ -22,5 +22,11 @@ public class EmployeePayrollServiceTest {
 		long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_I0);
 		assertEquals( 3, entries);
 	}
-
+	
+	@Test
+		public void givenFileOnReadingFromFileShouldMatchEmployeeCount() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		long entries = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.FILE_I0);
+		assertEquals(3, entries);
+	}
 }
