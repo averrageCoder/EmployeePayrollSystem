0f38a5d48d17260066db039c80177ff004537b78
Harsh R Jain <rjainharsh@gmail.com>
2021-09-25 21:56:16 +0530

    MODIFIED: start date field added

--- lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.employeepayroll;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
 	
 	public int id;
 	public String name;
 	public double salary;
+	public LocalDate startDate;
 	
 	public EmployeePayrollData(int id, String name, double salary) {
 		this.id = id;
@@ -12,9 +15,14 @@ public class EmployeePayrollData {
 		this.salary = salary;
 	}
 
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+		this(id,name,salary);
+		this.startDate = startDate;
+	}
+
 	@Override
 	public String toString() {
-		return "id=" + id + ", name=" + name + ", salary=" + salary;
+		return "id=" + id + ", name=" + name + ", salary=" + salary + ", startDate=" + startDate;
 	}
 	
 }
